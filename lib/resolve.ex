14f5de1be5b83ee4e7a2948d724fdea8765b62fa
RickR <rrein@akamai.com>
2015-07-15 01:14:41 -0400

    build label as we recurse

--- lib/resolve.ex
+++ lib/resolve.ex
@@ -1,6 +1,11 @@
 defmodule ExDns.Resolve do
   @root_hints_url 'http://www.internic.net/domain/named.root'
 
+  def random(list) do
+    :random.seed(:erlang.now)
+    Enum.at(list, :random.uniform(length(list)) - 1)
+  end
+
   def root_servers do
     #["198.41.0.4", "192.228.79.201"]
     ["198.41.0.4"]
@@ -10,32 +15,39 @@ defmodule ExDns.Resolve do
   # but when being called it would look like ExDns.Resolve.labels(labels)
   # which kind of makes sense
   def labels(labels) do
-    ip = labels |> fetch_ns_record |> fetch_a_record
+    ip = labels
+      |> fetch_ns_record
+      |> fetch_a_record
     IO.puts "FINAL IP: #{inspect(ip)}"
     ip
   end
 
   def fetch_ns_record(labels) do
-    IO.puts(inspect(labels))
-    ns_ip = List.foldr(labels, nil, fn(label, auth_server) -> ask_for_authoritative(auth_server, label) end)
-    [label: List.first(labels), ns_ip: ns_ip]
+    List.foldr(labels, {}, fn(cur_label, ns_ip_and_label) -> ask_for_authoritative(ns_ip_and_label, cur_label) end)
   end
 
-  def fetch_a_record(auth_server) do
-    IO.puts("Asking #{auth_server[:ns_ip]} for label #{auth_server[:label]} (A)")
-    "1.2.3.4"
-  end
-
-  def ask_for_authoritative(auth_server, label) do
-    ns_server = case auth_server do
-      nil -> random(root_servers)
-      _ -> "1.2.3.4"
+  def ask_for_authoritative(ns_ip_and_label, cur_label) do
+    {ns_ip, full_label} = case ns_ip_and_label do
+      {} -> {random(root_servers), ""}
+      {_, _} -> ns_ip_and_label
     end
 
-    IO.puts("Asking #{ns_server} for label: #{label} (NS)")
+    # This is kind of crappy. On our initial run we pass in an empty tuple.
+    # Above we match on an empty tuple and set full_label to an empty string.
+    # This gives us the result of appending a "." to our initial label.
+    label = "#{cur_label}.#{full_label}"
+
+    IO.puts("Asking #{ns_ip} for label: #{label} (NS)")
     ExDns.Request.build(label, "ns")
-      |> send_request(ns_server)
+      |> send_request(ns_ip)
       |> close_socket
+    {ns_ip, label}
+  end
+
+  def fetch_a_record(auth_ip_and_label) do
+    {auth_ip, label} = auth_ip_and_label
+    IO.puts("Asking #{auth_ip} for label #{label} (A)")
+    "1.2.3.4"
   end
 
   def send_request(request, ns_server) do
@@ -50,11 +62,5 @@ defmodule ExDns.Resolve do
 
   def close_socket(socket) do
     :gen_udp.close(socket)
-  end
-
-
-  def random(list) do
-    :random.seed(:erlang.now)
-    Enum.at(list, :random.uniform(length(list)) - 1)
   end
 end
